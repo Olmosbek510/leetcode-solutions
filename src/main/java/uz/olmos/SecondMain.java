996ffacb2a2c22aedd7f33ef8582c9567eac9e91
Olmosbek510 <olmosbekorazboyev@gmail.com>
2024-08-01 14:16:33 +0500

    31 july 4 problems solved in total

--- src/main/java/uz/olmos/SecondMain.java
+++ src/main/java/uz/olmos/SecondMain.java
@@ -10,6 +10,10 @@ public class SecondMain {
         System.out.println(findLucky(new int[]{8}));
         System.out.println(Arrays.toString(findIndices(new int[]{3}, 1, 1)));
         System.out.println(lastVisitedIntegers(new int[]{1, -1, 2, -1, -1}));
+        System.out.println(sortSentence("is2 sentence4 This1 a3"));
+        System.out.println(maxProductDifference(new int[]{2, 9, 5, 9, 1}));
+        System.out.println(canBeIncreasing(new int[]{2, 3, 1, 2}));
+        System.out.println(isSorted(List.of(2, 4, 20, 19)));
     }
 
     //    1436. Destination City
@@ -111,5 +115,69 @@ public class SecondMain {
         }
         return ans;
     }
+
+    public static String sortSentence(String s) {
+        String[] strArr = s.split(" ");
+        int n = strArr.length;
+        StringJoiner result = new StringJoiner(" ");
+
+        int min;
+        for (int i = 0; i < n - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (getDigit(strArr[j]) < getDigit(strArr[min])) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                String temp = strArr[min];
+                strArr[min] = strArr[i];
+                strArr[i] = temp;
+            }
+            result.add(strArr[i].substring(0, strArr[i].length() - 1));
+        }
+        String last = strArr[strArr.length - 1];
+        result.add(last.substring(0, last.length() - 1));
+        return result.toString();
+    }
+
+    public static Integer getDigit(String str) {
+        char c = str.charAt(str.length() - 1);
+        System.out.println(c);
+        return Integer.parseInt(String.valueOf(c));
+    }
+
+    //        1913. Maximum Product Difference Between Two Pairs
+    public static int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]);
+    }
+
+    public static boolean canBeIncreasing(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    temp.add(nums[j]);
+                }
+            }
+            if (isSorted(temp)) return true;
+        }
+        return false;
+    }
+
+    public static boolean isSorted(List<Integer> nums) {
+        int ctr = 0;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (nums.get(i) < nums.get(i + 1)) {
+                ctr++;
+            }
+        }
+        return ctr == n - 1;
+    }
+
+
 
 }
